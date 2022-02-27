2fb1206f8d5b99650bfd52e08069d0d4faa1d623
Roy Dyck <dyck.ro@gmail.com>
2022-02-27 13:05:10 -0600

    Add function code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = '';
+    for (let i = 1; i <= string.length; i++) {
+        reverse = reverse.concat('',string.substr(-i,1));
+    }
+    return reverse;
 };
 
 // Do not edit below this line
