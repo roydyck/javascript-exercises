7cb6e1653a86ced0f826e3f463eabfeee701e981
Roy Dyck <dyck.ro@gmail.com>
2022-02-27 14:55:49 -0600

    Add function code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    let args = Array.prototype.slice.call(arguments);
+    let argArray = args[0];
+    
+    for (let i = 1; i < args.length; i++) {
+        for (let j = 0; j < argArray.length; j++) {
+            if (argArray[j] === args[i]) {
+                argArray.splice(j,1);
+            }
+        }
+    }
 
+    return argArray;
 };
 
 // Do not edit below this line
