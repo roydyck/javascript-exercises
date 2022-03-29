01bc1da8c47a2e43505143e9082bdf7b41314e56
Roy Dyck <dyck.ro@gmail.com>
2022-03-28 21:48:03 -0500

    Add remaining function code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,27 +6,33 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(...args) {
+const sum = function(args) {
+ 
   if (!args) {
     return 0;
   }
 
-	let total = 0;
-
-  args.forEach(element => total + element);
-  return total;
+  return args.reduce((total, element) => total + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((total, element) => total * element, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a===0) {
+    return 1;
+  }
+
+  let factorial = 1;
+  for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
