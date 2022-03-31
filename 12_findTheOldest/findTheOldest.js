5d43d8cd2ae997b5541778ceaa7cb5d086c56186
Roy Dyck <dyck.ro@gmail.com>
2022-03-30 22:01:49 -0500

    Write code for oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
 
+    people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))
+    return people[0];
 };
 
 // Do not edit below this line
