210d4dcaa485789f0bb56573a11ba015366ce947
Roy Dyck <dyck.ro@gmail.com>
2022-03-30 21:13:08 -0500

    Fix space skipping bug
    Added a for loop decrement when the if statement was true

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordArray = word.split('');
+    
+    for (let i = 0; i < wordArray.length; i++) {
+        if ((wordArray[i].toLowerCase() === wordArray[i].toUpperCase()) || wordArray[i] === '') {
+            wordArray.splice(i,1);
+            i--;
+        } else {
+            wordArray[i] = wordArray[i].toLowerCase();
+        }
+    }
+    const reverseWord = wordArray.reverse().join('');
+    wordArray = wordArray.reverse().join('');
 
+    return reverseWord === wordArray;
 };
 
 // Do not edit below this line
