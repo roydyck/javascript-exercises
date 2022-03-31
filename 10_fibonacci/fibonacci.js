e43c6452915909270d56e27d84e2647bdc53890d
Roy Dyck <dyck.ro@gmail.com>
2022-03-30 21:39:18 -0500

    Add fibonacci function code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) return "OOPS";
 
+    let fib1 = 0;
+    let fib2 = 1;
+
+    let fib3 = 1; // in case input is 1st fibonacci number
+
+    for (let i = 1; i < input; i++) { // if 1st fibonacci number is input, skip the for loop
+        fib3 = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib3;
+    }
+    return fib3;
 };
 
 // Do not edit below this line
